df7d5e30c2b6e5e0f90b44cd528960bffe44179a
BoopalanA <80742111+BoopalanA@users.noreply.github.com>
2024-10-23 13:21:14 +0530

    Create SQL syntax.sql

---
+++ SQL syntax.sql
@@ -0,0 +1,25 @@
+https://docs.snowflake.com/en/sql-reference/constructs
+
+[ WITH ... ]
+SELECT
+   [ TOP <n> ]
+   ...
+[ INTO ... ]
+[ FROM ...
+   [ AT | BEFORE ... ]
+   [ CHANGES ... ]
+   [ CONNECT BY ... ]
+   [ JOIN ... ]
+   [ ASOF JOIN ... ]
+   [ LATERAL ... ]
+   [ MATCH_RECOGNIZE ... ]
+   [ PIVOT | UNPIVOT ... ]
+   [ VALUES ... ]
+   [ SAMPLE ... ] ]
+[ WHERE ... ]
+[ GROUP BY ...
+   [ HAVING ... ] ]
+[ QUALIFY ... ]
+[ ORDER BY ... ]
+[ LIMIT ... ]
+[ FOR UPDATE ... ]
